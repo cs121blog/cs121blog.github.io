4e1cde2bba5b9bd0acb77e2466874a42a9331300
imgr2net <rsreland@hotmail.com>
2026-05-05 13:12:19 +0800

    Site updated: 2026-05-05 13:12:09

--- js/duration.js
+++ js/duration.js
@@ -20,7 +20,7 @@
       if(String(snum).length === 1 ){
         snum = "0" + snum;
       }
-      document.getElementById("timeDate").innerHTML = "「百二一」从2014年开始，已运行了&nbsp"+dnum+"&nbsp天。";
+      document.getElementById("timeDate").innerHTML = "「呼号121」从2014年开始，已运行了&nbsp"+dnum+"&nbsp天。";
       /* document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒"; */
     }
     setInterval(update, 1000);
